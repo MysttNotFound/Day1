a90eaa5aaf8779348372e97c95bfd3ed9a483d8d
Anshuman Singh <anshuman9147@gmail.com>
2026-06-07 10:21:02 +0530

    bestTimeToBuyAndSellStocks

---
+++ bestTimeToBuyAndSellStocks.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxprofit=0;
+        for(int i=0;i<prices.length;i++){
+            for(int j=i+1;j<prices.length;j++){
+                if(prices[i]<prices[j]){
+                    int profit = prices[j]-prices[i];
+                    if(profit>maxprofit){
+                        maxprofit = profit;
+                    }
+                }
+            }
+        }
+        return maxprofit;
+    }
+}
---
+++ removeDuplicatesFromSortedArray
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int rd = 0;
+        for(int i = 0;i<nums.length;i++){
+            if(nums[rd]!=nums[i]){
+                rd++;
+             nums[rd]=nums[i];    
+            }
+            else{ 
+            continue;
+            }
+        }
+     return rd+1;
+    }
+}
